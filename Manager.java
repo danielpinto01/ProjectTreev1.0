08951cc57848d060fd85b00017ea3c9e5f382ddd
danielpinto01 <36775347+danielpinto01@users.noreply.github.com>
2018-04-12 14:40:56 -0500

    Se conectó la clase Tree con la clase Manager

---
+++ Manager.java
@@ -0,0 +1,72 @@
+package models;
+
+import java.io.File;
+
+public class Manager {
+	
+	private File fatherFile;
+	private Tree treeFiles;
+
+	public Manager(String fatherPath, int sizeFile) {
+		fatherFile = new File(fatherPath);
+		treeFiles = new Tree(fatherFile, sizeFile);
+		
+	}
+	
+	public Manager() {
+		treeFiles = new Tree();
+	}
+	
+	public void addToTree() {
+		addToTree(fatherFile);
+	}
+	
+	private void addToTree(File actual){
+		treeFiles.addFiles(actual);
+		if (actual.isDirectory()) {
+			for (File file : actual.listFiles()) {
+				addToTree(file);
+			}
+		}
+	}
+	
+	public void showInformationFile(){
+		showInformationFile(fatherFile, " ");
+	}
+
+	private void showInformationFile(File actual, String empty) {
+		System.out.println(empty + actual.getName());
+		if (actual.isDirectory()) {
+			for (File file : actual.listFiles()) {
+				showInformationFile(file, empty + "		|"); 
+			}
+		}
+	}
+
+	public File getFatherFile() {
+		return fatherFile;
+	}
+	
+	public void showTree(){
+		treeFiles.showTree();
+	}
+	
+	public void setFileFather(File father){
+		this.fatherFile = father;
+		treeFiles = new Tree(father);
+		addToTree();
+	}
+	
+	public void setTotalSize(int totalSize){
+		treeFiles.clearTree();
+		treeFiles.setTotalSize(totalSize);
+		addToTree();
+	}
+	
+	public Node getRootTree(){
+		return treeFiles.getRoot();
+	}
+	public Tree getFiles() {
+		return treeFiles;
+	}
+}
